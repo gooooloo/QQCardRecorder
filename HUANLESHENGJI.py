d5aedb2fd891db9791bd80d3e550f5a0ba838e1a
gooooloo <qidu.lin.dev@gmail.com>
2013-12-18 20:48:54 +0800

    keeping query memory in background

--- HUANLESHENGJI.py
+++ HUANLESHENGJI.py
@@ -1,5 +1,6 @@
 from ctypes import *
 from ctypes.wintypes import *
+from time import sleep
 import psutil
 
 def getPid():
@@ -18,12 +19,13 @@ PROCESS_ALL_ACCESS = 0x1F0FFF
 DIPAI_ADDRESS = 0x004AE01E
 MY_CARD_ADDRESS = 0x4ae586
 LastPlayedBase = 0x4abe02+0x100-4
-MyLastPlayed = LastPlayedBase+0x78
-ShangJiaLastPlayed = LastPlayedBase+0x330
-DuiJiaLastPlayed = LastPlayedBase+0x5e8
-XiaJiaLastPlayed = LastPlayedBase+0x8a0
+MyLastPlayed = LastPlayedBase+0xb58
+ShangJiaLastPlayed = LastPlayedBase+0xe10
+DuiJiaLastPlayed = LastPlayedBase+0x10c8
+XiaJiaLastPlayed = LastPlayedBase+0x1380
 LAST_ROUND_CARDS_NUMBER_ADDRESS = 0x4acca8
 MY_LEFT_CARD_NUMBER_ADDRESS = 0x4ae7d8
+
 
 DIVIDER_LINE = "======"
 
@@ -34,19 +36,6 @@ ival = c_int()
 cval = c_char()
 bufferSize = len(buffer.value)
 bytesRead = c_ulong(0)
-
-processHandle = OpenProcess(PROCESS_ALL_ACCESS, False, pid)
-
-'''
-def printHuanLeDou():
-    print("欢乐豆")
-    address = 0x#TODO
-    if ReadProcessMemory(processHandle, address, buffer, bufferSize, byref(bytesRead)):
-        memmove(byref(ival), buffer, sizeof(ival))
-        print("Success:" + str(ival.value))
-    else:
-        print("Failed.")
-'''
 
 huase = {
         b'\x00':'',
@@ -85,16 +74,22 @@ def f(address, g):
 def getCardsByAddressAndLen(pp, len):
     return ' '.join([''.join([f(pp+8*i, huase),f(pp+8*i+1, dianshu)]) for i in range(0, len)])
 
-print("底牌", getCardsByAddressAndLen(DIPAI_ADDRESS, 8), DIVIDER_LINE, sep='\n')
-n = f(MY_LEFT_CARD_NUMBER_ADDRESS, ord)
-print("我的牌", getCardsByAddressAndLen(MY_CARD_ADDRESS, n), DIVIDER_LINE, sep='\n')
-n = f(LAST_ROUND_CARDS_NUMBER_ADDRESS, ord)
-print("上一轮出牌数", n, sep=' ')
-print("我   ", getCardsByAddressAndLen(MyLastPlayed, n), sep=' ')
-print("下家 ", getCardsByAddressAndLen(XiaJiaLastPlayed, n), sep=' ')
-print("对家 ", getCardsByAddressAndLen(DuiJiaLastPlayed, n), sep=' ')
-print("上家 ", getCardsByAddressAndLen(ShangJiaLastPlayed, n), sep=' ')
-
-
+processHandle = OpenProcess(PROCESS_ALL_ACCESS, False, pid)
+myleftcnt = 0
+while 1==1:
+    sleep(0.050)
+    n = f(MY_LEFT_CARD_NUMBER_ADDRESS, ord)
+    if n != myleftcnt:
+        myleftcnt = n
+        print("底牌", getCardsByAddressAndLen(DIPAI_ADDRESS, 8), DIVIDER_LINE, sep='\n')
+        print("我的牌", getCardsByAddressAndLen(MY_CARD_ADDRESS, myleftcnt), DIVIDER_LINE, sep='\n')
+        n = f(LAST_ROUND_CARDS_NUMBER_ADDRESS, ord)
+        print("上一轮出牌数", n, sep=' ')
+        print("我   ", getCardsByAddressAndLen(MyLastPlayed, n), sep=' ')
+        print("下家 ", getCardsByAddressAndLen(XiaJiaLastPlayed, n), sep=' ')
+        print("对家 ", getCardsByAddressAndLen(DuiJiaLastPlayed, n), sep=' ')
+        print("上家 ", getCardsByAddressAndLen(ShangJiaLastPlayed, n), sep=' ')
 CloseHandle(processHandle)
+
+
 
