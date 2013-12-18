76b53fa8724479c7bbffd466f8d59aea4eb47f92
gooooloo <qidu.lin.dev@gmail.com>
2013-12-18 20:13:28 +0800

    improvement

--- HUANLESHENGJI.py
+++ HUANLESHENGJI.py
@@ -22,6 +22,8 @@ MyLastPlayed = LastPlayedBase+0x78
 ShangJiaLastPlayed = LastPlayedBase+0x330
 DuiJiaLastPlayed = LastPlayedBase+0x5e8
 XiaJiaLastPlayed = LastPlayedBase+0x8a0
+LAST_ROUND_CARDS_NUMBER_ADDRESS = 0x4acca8
+MY_LEFT_CARD_NUMBER_ADDRESS = 0x4ae7d8
 
 DIVIDER_LINE = "======"
 
@@ -47,11 +49,11 @@ def printHuanLeDou():
 '''
 
 huase = {
-        b'\x00':'无',
-        b'\x01':'  黑',
-        b'\x02':'    红',
-        b'\x03':'      花',
-        b'\x04':'        方'
+        b'\x00':'',
+        b'\x01':'黑',
+        b'\x02':'红',
+        b'\x03':'花',
+        b'\x04':'方'
         }
 
 dianshu = {
@@ -76,20 +78,22 @@ dianshu = {
 def f(address, g):
     if ReadProcessMemory(processHandle, address, buffer, bufferSize, byref(bytesRead)):
         memmove(byref(cval), buffer, sizeof(cval))
-        return g[cval.value]
+        return g[cval.value] if type(g)==dict else g(cval.value) if hasattr(g, '__call__') else cval.value
     else:
         return "Failed."
 
 def getCardsByAddressAndLen(pp, len):
-    return '\n'.join(['   '.join([f(pp+8*i, huase),f(pp+8*i+1, dianshu)]) for i in range(0, len)])
+    return ' '.join([''.join([f(pp+8*i, huase),f(pp+8*i+1, dianshu)]) for i in range(0, len)])
 
 print("底牌", getCardsByAddressAndLen(DIPAI_ADDRESS, 8), DIVIDER_LINE, sep='\n')
-print("我的牌", getCardsByAddressAndLen(MY_CARD_ADDRESS, 25+8), DIVIDER_LINE, sep='\n')
-print("上一轮我出牌", getCardsByAddressAndLen(MyLastPlayed, 5), sep='\n')
-print("上一轮下家出牌", getCardsByAddressAndLen(XiaJiaLastPlayed, 5), sep='\n')
-print("上一轮对家出牌", getCardsByAddressAndLen(DuiJiaLastPlayed, 5), sep='\n')
-print("上一轮上家出牌", getCardsByAddressAndLen(ShangJiaLastPlayed, 5), sep='\n')
-
+n = f(MY_LEFT_CARD_NUMBER_ADDRESS, ord)
+print("我的牌", getCardsByAddressAndLen(MY_CARD_ADDRESS, n), DIVIDER_LINE, sep='\n')
+n = f(LAST_ROUND_CARDS_NUMBER_ADDRESS, ord)
+print("上一轮出牌数", n, sep=' ')
+print("我   ", getCardsByAddressAndLen(MyLastPlayed, n), sep=' ')
+print("下家 ", getCardsByAddressAndLen(XiaJiaLastPlayed, n), sep=' ')
+print("对家 ", getCardsByAddressAndLen(DuiJiaLastPlayed, n), sep=' ')
+print("上家 ", getCardsByAddressAndLen(ShangJiaLastPlayed, n), sep=' ')
 
 
 CloseHandle(processHandle)
